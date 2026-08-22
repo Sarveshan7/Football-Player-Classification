27c049ce573a97f301f38f5318ca4f0312edebd7
Sarveshan7 <sarveshansilvam@gmail.com>
2026-08-22 12:21:55 +1000

    Uploading updated files

--- UI/app.js
+++ UI/app.js
@@ -1,74 +1,93 @@
 Dropzone.autoDiscover = false;
 
+const playerMeta = {
+    "cristiano_ronaldo": { name: "Cristiano Ronaldo", img: "./images/ronaldo.jpg" },
+    "erling_haaland":    { name: "Erling Haaland",    img: "./images/haaland.jpg" },
+    "kylian_mbappe":     { name: "Kylian Mbapp\u00e9", img: "./images/mbappe.jpg" },
+    "lionel_messi":      { name: "Lionel Messi",       img: "./images/messi.jpg" }
+};
+
 function init() {
     let dz = new Dropzone("#dropzone", {
         url: "/",
         maxFiles: 1,
         addRemoveLinks: true,
-        dictDefaultMessage: "Some Message",
+        dictDefaultMessage: "",
         autoProcessQueue: false
     });
-    
-    dz.on("addedfile", function() {
-        if (dz.files[1]!=null) {
-            dz.removeFile(dz.files[0]);        
+
+    dz.on("addedfile", function () {
+        if (dz.files[1] != null) {
+            dz.removeFile(dz.files[0]);
         }
     });
 
     dz.on("complete", function (file) {
-        let imageData = file.dataURL;
-        
-        var url = "http://127.0.0.1:5000/classify_image";
+        let url = "http://127.0.0.1:5000/classify_image";
 
         $.post(url, {
             image_data: file.dataURL
-        },function(data, status) {
-            
+        }, function (data, status) {
+
             console.log(data);
-            if (!data || data.length==0) {
-                $("#resultHolder").hide();
-                $("#divClassTable").hide();                
+            if (!data || data.length == 0) {
+                $("#result").hide();
                 $("#error").show();
                 return;
             }
-            let players = ["cristiano_ronaldo", "erling_haaland", "kylian_mbappe", "lionel_messi"];
-            
+
             let match = null;
             let bestScore = -1;
-            for (let i=0;i<data.length;++i) {
+            for (let i = 0; i < data.length; ++i) {
                 let maxScoreForThisClass = Math.max(...data[i].class_probability);
-                if(maxScoreForThisClass>bestScore) {
+                if (maxScoreForThisClass > bestScore) {
                     match = data[i];
                     bestScore = maxScoreForThisClass;
                 }
             }
+
             if (match) {
                 $("#error").hide();
-                $("#resultHolder").show();
-                $("#divClassTable").show();
-                $("#resultHolder").html($(`[data-player="${match.class}"`).html());
+
+                let meta = playerMeta[match.class] || { name: match.class, img: "" };
+                $("#result-img").attr("src", meta.img).attr("alt", meta.name);
+                $("#result-name").text(meta.name);
+                $("#result-conf").text(Math.round(bestScore) + "% confidence");
+
                 let classDictionary = match.class_dictionary;
-                for(let personName in classDictionary) {
+                let rows = Object.keys(classDictionary).map(function (personName) {
                     let index = classDictionary[personName];
-                    let proabilityScore = match.class_probability[index];
-                    let elementName = "#score_" + personName;
-                    $(elementName).html(proabilityScore);
-                }
+                    let score = match.class_probability[index];
+                    return { key: personName, score: score };
+                });
+                rows.sort(function (a, b) { return b.score - a.score; });
+
+                let barsHtml = rows.map(function (row, i) {
+                    let meta = playerMeta[row.key] || { name: row.key };
+                    let pct = Math.max(0, Math.min(100, Math.round(row.score)));
+                    let leadClass = i === 0 ? " lead" : "";
+                    return (
+                        '<div class="bar-row">' +
+                            '<div class="bar-name">' + meta.name.split(" ").pop() + '</div>' +
+                            '<div class="bar-track"><div class="bar-fill' + leadClass + '" style="width:' + pct + '%"></div></div>' +
+                            '<div class="bar-pct">' + pct + '%</div>' +
+                        '</div>'
+                    );
+                }).join("");
+                $("#bars").html(barsHtml);
+
+                $("#result").show();
             }
-            // dz.removeFile(file);            
         });
     });
 
-    $("#submitBtn").on('click', function (e) {
-        dz.processQueue();		
+    $("#submitBtn").on('click', function () {
+        dz.processQueue();
     });
 }
 
-$(document).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
     $("#error").hide();
-    $("#resultHolder").hide();
-    $("#divClassTable").hide();
-
+    $("#result").hide();
     init();
 });
